17aa405c17a2bc08b03181d2cb7ae83510bbfe09
tdm <55875354+taraDM23@users.noreply.github.com>
2019-11-09 20:42:29 +1100

    fixed local storage bug

--- script.js
+++ script.js
@@ -52,46 +52,47 @@ for (i = 9; i <= 17; i++) {
 function ReloadSavedData() {
     console.log("test")
     var getItem = JSON.parse(localStorage.getItem("items"));
-    console.log(getItem);
-    console.log(getItem.AddText9);
-    if (typeof getItem.AddText9 !== 'undefined' && getItem.AddText9 !== null) {
-        document.getElementById("AddText9").value = getItem.AddText9;
-        console.log(getItem.AddText9)
-    }
-    if (typeof getItem.AddText10 !== 'undefined' && getItem.AddText10 !== null) {
-        document.getElementById("AddText10").value = getItem.AddText10;
-        console.log(getItem.AddText10)
-    }
-    if (typeof getItem.AddText11 !== 'undefined' && getItem.AddText11 !== null) {
-        document.getElementById("AddText11").value = getItem.AddText11;
-        console.log(getItem.AddText11)
-    }
-    if (typeof getItem.AddText12 !== 'undefined' && getItem.AddText12 !== null) {
-        document.getElementById("AddText12").value = getItem.AddText12;
-        console.log(getItem.AddText12)
-    }
-    if (typeof getItem.AddText13 !== 'undefined' && getItem.AddText13 !== null) {
-        document.getElementById("AddText13").value = getItem.AddText13;
-        console.log(getItem.AddText13)
-    }
-    if (typeof getItem.AddText14 !== 'undefined' && getItem.AddText14 !== null) {
-        document.getElementById("AddText14").value = getItem.AddText14;
-        console.log(getItem.AddText14)
-    }
-    if (typeof getItem.AddText15 !== 'undefined' && getItem.AddText15 !== null) {
-        document.getElementById("AddText15").value = getItem.AddText15;
-        console.log(getItem.AddText15)
-    }
-    if (typeof getItem.AddText17 !== 'undefined' && getItem.AddText17 !== null) {
-        document.getElementById("AddText17").value = getItem.AddText17;
-        console.log(getItem.AddText17)
-    }
-    if (typeof getItem.AddText16 !== 'undefined' && getItem.AddText16 !== null) {
-        document.getElementById("AddText16").value = getItem.AddText16;
-        console.log(getItem.AddText16)
+    if (getItem !== null) {
+        console.log(getItem);
+        console.log(getItem.AddText9);
+        if (typeof getItem.AddText9 !== 'undefined' && getItem.AddText9 !== null) {
+            document.getElementById("AddText9").value = getItem.AddText9;
+            console.log(getItem.AddText9)
+        }
+        if (typeof getItem.AddText10 !== 'undefined' && getItem.AddText10 !== null) {
+            document.getElementById("AddText10").value = getItem.AddText10;
+            console.log(getItem.AddText10)
+        }
+        if (typeof getItem.AddText11 !== 'undefined' && getItem.AddText11 !== null) {
+            document.getElementById("AddText11").value = getItem.AddText11;
+            console.log(getItem.AddText11)
+        }
+        if (typeof getItem.AddText12 !== 'undefined' && getItem.AddText12 !== null) {
+            document.getElementById("AddText12").value = getItem.AddText12;
+            console.log(getItem.AddText12)
+        }
+        if (typeof getItem.AddText13 !== 'undefined' && getItem.AddText13 !== null) {
+            document.getElementById("AddText13").value = getItem.AddText13;
+            console.log(getItem.AddText13)
+        }
+        if (typeof getItem.AddText14 !== 'undefined' && getItem.AddText14 !== null) {
+            document.getElementById("AddText14").value = getItem.AddText14;
+            console.log(getItem.AddText14)
+        }
+        if (typeof getItem.AddText15 !== 'undefined' && getItem.AddText15 !== null) {
+            document.getElementById("AddText15").value = getItem.AddText15;
+            console.log(getItem.AddText15)
+        }
+        if (typeof getItem.AddText17 !== 'undefined' && getItem.AddText17 !== null) {
+            document.getElementById("AddText17").value = getItem.AddText17;
+            console.log(getItem.AddText17)
+        }
+        if (typeof getItem.AddText16 !== 'undefined' && getItem.AddText16 !== null) {
+            document.getElementById("AddText16").value = getItem.AddText16;
+            console.log(getItem.AddText16)
+        }
     }
 }
-
 
 // Melb temp section
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
