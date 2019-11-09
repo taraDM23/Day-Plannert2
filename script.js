9a683b8f44c43218cd1c5f7447fc2d5c548886ca
tdm <55875354+taraDM23@users.noreply.github.com>
2019-11-09 20:04:00 +1100

    fixed bug

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ var values = {};
 for (i = 9; i <= 17; i++) {
     let SaveBtn = document.getElementById("save" + i);
     let AddText = document.getElementById("AddText" + i);
+    values[AddText.id] = AddText.value;
 
     SaveBtn.addEventListener("click", function SaveEvent() {
         console.log(this);
@@ -54,42 +55,42 @@ function ReloadSavedData() {
     var getItem = JSON.parse(localStorage.getItem("items"));
     console.log(getItem);
     console.log(getItem.AddText9);
-    if (getItem.AddText9 !== null) {
+    if (typeof getItem.AddText9 !== 'undefined' && getItem.AddText9 !== null) {
         document.getElementById("AddText9").value = getItem.AddText9;
         console.log(getItem.AddText9)
     }
-    if (getItem.AddText10 !== null) {
+    if (typeof getItem.AddText10 !== 'undefined' && getItem.AddText10 !== null) {
         document.getElementById("AddText10").value = getItem.AddText10;
         console.log(getItem.AddText10)
     }
-    if (getItem.AddText11 !== null) {
+    if (typeof getItem.AddText11 !== 'undefined' && getItem.AddText11 !== null) {
         document.getElementById("AddText11").value = getItem.AddText11;
         console.log(getItem.AddText11)
     }
-    if (getItem.AddText12 !== null) {
+    if (typeof getItem.AddText12 !== 'undefined' && getItem.AddText12 !== null) {
         document.getElementById("AddText12").value = getItem.AddText12;
         console.log(getItem.AddText12)
     }
-    if (getItem.AddText13 !== null) {
+    if (typeof getItem.AddText13 !== 'undefined' && getItem.AddText13 !== null) {
         document.getElementById("AddText13").value = getItem.AddText13;
         console.log(getItem.AddText13)
     }
-    if (getItem.AddText14 !== null) {
+    if (typeof getItem.AddText14 !== 'undefined' && getItem.AddText14 !== null) {
         document.getElementById("AddText14").value = getItem.AddText14;
         console.log(getItem.AddText14)
     }
-    if (getItem.AddText15 !== null) {
+    if (typeof getItem.AddText15 !== 'undefined' && getItem.AddText15 !== null) {
         document.getElementById("AddText15").value = getItem.AddText15;
         console.log(getItem.AddText15)
     }
-    if (getItem.AddText17 !== null) {
+    if (typeof getItem.AddText17 !== 'undefined' && getItem.AddText17 !== null) {
         document.getElementById("AddText17").value = getItem.AddText17;
         console.log(getItem.AddText17)
     }
-    if (getItem.AddText16 !== null) {
+    if (typeof getItem.AddText16 !== 'undefined' && getItem.AddText16 !== null) {
         document.getElementById("AddText16").value = getItem.AddText16;
         console.log(getItem.AddText16)
-    } else(getItem.AddText16 == null)
+    }
 }
 
 
